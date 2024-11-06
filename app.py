ee8dbd39e982c2eb4529aab9a9e933915089a957
Nentozet <160948397+Nentozet@users.noreply.github.com>
2024-11-06 16:31:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_PATH')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
