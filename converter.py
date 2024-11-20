44dfcc5349375a38b180acd73c7a12d3165d185e
Nentozet <160948397+Nentozet@users.noreply.github.com>
2024-11-20 21:24:20 +0300

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,4 +1,6 @@
 from toolset import Toolset
+from config import Config
+from decimal import Decimal, getcontext
 
 
 class Converter:
@@ -6,9 +8,21 @@ class Converter:
 
     @staticmethod
     def get_converted_number(number, from_base, to_base):
-        number_in_decimal = int(str(number), from_base)
+        getcontext().prec = Config.Conversion_Accuracy * 4
+        number = Converter.__get_validated_number(number)
 
-        return Converter.__change_base_from_dec(number_in_decimal, to_base)
+        if number[0] == "-":
+            negative = "-"
+            number = number[1:]
+        else:
+            negative = ""
+
+        if from_base != 10:
+            number_dec = str(Converter.__change_base_to_dec(number, from_base))
+        else:
+            number_dec = number
+
+        return negative + Converter.__change_base_from_dec(number_dec, to_base)
 
     @staticmethod
     def get_calculated_number(number1, base1, number2, base2, calculation_base, operation):
@@ -16,17 +30,100 @@ class Converter:
         number2 = Converter.get_converted_number(number2, base2, 10)
 
         expression = f' {operation} '.join((number1, number2))
-        calculation_result_dec = round(eval(expression), 4)
+        calculation_result_dec = str(eval(expression))
 
-        calculation_result = Converter.get_converted_number(calculation_result_dec, 10, calculation_base)
+        if calculation_base != 10:
+            calculation_result = Converter.get_converted_number(calculation_result_dec, 10, calculation_base)
+        else:
+            calculation_result = calculation_result_dec
+
+        if '.' in calculation_result:
+            calculation_result = calculation_result.rstrip('0')
 
         calculation_result_format = Toolset.get_number_with_base(calculation_result, calculation_base)
 
         return calculation_result_format
 
     @staticmethod
-    def __change_base_from_dec(number, base):
-        if number < base:
-            return Converter.digits[number]
+    def __change_base_to_dec(number, base):
+        if '.' in number:
+            integer_part, frac_part = number.split('.')
         else:
-            return Converter.__change_base_from_dec(number // base, base) + Converter.digits[number % base]
+            integer_part, frac_part = number, ''
+
+        integer_value = 0
+        for i, digit in enumerate(reversed(integer_part)):
+            integer_value += int(digit, base) * (base ** i)
+
+        fractional_value = Decimal(0)
+        if frac_part:
+            for i, digit in enumerate(frac_part, start=1):
+                fractional_value += Decimal(int(digit, base)) * (Decimal(1.0) / Decimal(base ** i))
+
+        return integer_value + fractional_value
+
+    @staticmethod
+    def __change_base_from_dec(number, base):
+        if base == 10:
+            return number
+
+        if '.' in number:
+            integer_part, fractional_part = number.split('.')
+        else:
+            integer_part, fractional_part = number, ''
+
+        # Преобразование целой части
+        integer_part = int(integer_part)
+        result_integer = ''
+        if integer_part == 0:
+            result_integer = '0'
+        else:
+            while integer_part > 0:
+                digit = integer_part % base
+                result_integer = Converter.digits[digit] + result_integer
+                integer_part //= base
+
+        # Преобразование дробной части
+        result_fractional = ''
+        if fractional_part:
+            fractional_part = Decimal(f'0.{fractional_part}')
+            for _ in range(Config.Conversion_Accuracy):  # Точность до Config.Conversion_Accuracy знаков
+                fractional_part *= base
+                digit = int(fractional_part)
+                result_fractional += Converter.digits[digit]
+                fractional_part -= digit
+                if fractional_part == 0:
+                    break
+
+        # Объединение результата
+        result = result_integer
+        if result_fractional:
+            result += '.' + result_fractional
+
+        return result
+
+    @staticmethod
+    def __get_validated_number(number):
+        number = number.replace(" ", "").replace(",", ".").lstrip("0")
+
+        if number.count(".") > 1:
+            raise ValueError
+
+        if number[0] == "-":
+            negative = "-"
+            number = number[1:]
+        else:
+            negative = ""
+
+        if '.' in number:
+            int_part, frac_part = number.split('.')
+            if int_part == '':
+                int_part = '0'
+            frac_part = frac_part.rstrip('0')
+        else:
+            int_part, frac_part = number, ''
+
+        if frac_part == '':
+            return negative + int_part
+        else:
+            return negative + int_part + '.' + frac_part
