10041b0b021a6811163e693cab71b9eeac54970f
Nentozet <160948397+Nentozet@users.noreply.github.com>
2024-11-20 21:24:38 +0300

    Update program.py

--- program.py
+++ program.py
@@ -1,6 +1,6 @@
-from flask import session, render_template
+from flask import render_template, flash
 from config import Config
-from user import db
+from user import UserManager, db
 from task import TaskManager
 import inspect
 import json
@@ -13,15 +13,44 @@ class Program:
 
     def __init__(self):
         self.task_manager = TaskManager()
+        self.user_manager = UserManager()
 
     @staticmethod
-    def init():
-        if not session.get("language") or not session.get("theme"):
-            session["language"] = Config.Default_Language
-            session["theme"] = Config.Default_Theme
+    def init(ses):
+        if not ses.get("language") or not ses.get("theme"):
+            ses["language"] = Config.Default_Language
+            ses["theme"] = Config.Default_Theme
+
+    def get_user(self, user_id):
+        return self.user_manager.get_user(user_id)
+
+    def create_user(self, username, password, ses):
+        self.user_manager.create_user(username, password, ses)
+
+    def check_registration_data(self, lang, username, password_1, password_2):
+        user = self.user_manager.get_user_by_name(username)
+        if user:
+            flash(self.Text_Base[lang]["existed_username_alert"], "danger")
+            return False
+
+        if password_1 != password_2:
+            flash(self.Text_Base[lang]["different_passwords_alert"], "danger")
+            return False
+
+        return True
+
+    def check_login_data(self, ses, username, password):
+        user = self.user_manager.get_user_by_name(username)
+        if user and user.check_password(password):
+            ses["user_id"] = user.id
+            ses["language"] = user.language
+            ses["theme"] = user.theme
+            return True
+
+        return False
 
     @staticmethod
-    def process_action(action, request, user=0):
+    def process_action(action, request, ses, user=0):
         match action:
             case None:
                 return False
@@ -31,14 +60,14 @@ class Program:
                     user.language = request.form.get("lang")
                     db.session.commit()
 
-                session["language"] = request.form.get("lang")
+                ses["language"] = request.form.get("lang")
                 return True
 
             case "switch_theme_light" | "switch_theme_dark":
                 if user:
                     user.theme = action.split("_")[-1]
                     db.session.commit()
-                session["theme"] = action.split("_")[-1]
+                ses["theme"] = action.split("_")[-1]
                 return True
 
             case _:
@@ -48,7 +77,10 @@ class Program:
         return self.task_manager.check_answer(answer, user)
 
     def reset_task(self, task_type, user):
-        self.task_manager.reset_task(task_type, user)
+        if user.need_to_reset_task:
+            self.task_manager.reset_task(task_type, user)
+            user.need_to_reset_task = False
+            db.session.commit()
 
     def get_task_text(self, user):
         return self.task_manager.get_text(self.Text_Base[user.language], user)
@@ -56,15 +88,15 @@ class Program:
     def get_task_result(self, user):
         return self.task_manager.get_result(self.Text_Base[user.language], user)
 
-    def get_rendered_template(self, filename, user=0, *args):
+    def get_rendered_template(self, filename, ses, user=0, *args):
         # !!! В качестве аргументов этой функции должны передаваться переменные с таким же названием, что и в html-файле
         if user:
             theme = user.theme
             language = user.language
             username_ = user.username
         else:
-            theme = session.get("theme")
-            language = session.get("language")
+            theme = ses.get("theme")
+            language = ses.get("language")
             username_ = 0
 
         data = {
@@ -82,3 +114,10 @@ class Program:
                 data[arg_name] = arg
 
         return render_template(filename, **data)
+
+    def on_solved_task(self, answer, user):
+        user.need_to_reset_task = True
+        db.session.commit()
+
+        res = self.check_answer_for_task(answer, user)
+        user.calculate_skill_score(res)
