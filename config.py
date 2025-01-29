f53604acfe219dd94b45a5949672a292ca77faf3
Nentozet <160948397+Nentozet@users.noreply.github.com>
2025-01-29 21:29:50 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,5 @@ class Config:
     Default_Language = "ru"
     Default_Theme = "dark"
     Correct_Font_Color = "#00b400"
-    Conversion_Precision = 12
+    Conversion_Accuracy = 12
+    Calculation_Accuracy = 12
