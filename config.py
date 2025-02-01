ae207d38efec8ea039cbe44986ae828fb77c044b
Nentozet <160948397+Nentozet@users.noreply.github.com>
2025-02-01 23:28:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,9 @@ class Config:
     Correct_Font_Color = "#00b400"
     Conversion_Accuracy = 12
     Calculation_Accuracy = 12
+    Operation_Changer = {
+        'add': '+',
+        'sub': '-',
+        'mult': '*',
+        'div': '/'
+    }
