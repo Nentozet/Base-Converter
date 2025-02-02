913c6e0fe7cf1042414fae0778e2271873fabdee
Nentozet <160948397+Nentozet@users.noreply.github.com>
2025-02-02 13:27:23 +0300

    Update toolset.py

--- toolset.py
+++ toolset.py
@@ -15,9 +15,16 @@ class Toolset:
         return str(number) + str(base).translate(base_table)
 
     @staticmethod
+    def get_validated_args(invalidated_dict, validating_dict):
+        validated_args = {}
+        for key in invalidated_dict.keys():
+            validated_args[validating_dict[key]] = invalidated_dict[key]
+        return validated_args
+
+    @staticmethod
     def keep_alive():
         try:
-            requests.get("https://base-converter.onrender.com")
+            requests.get("http://127.0.0.1:5000")
         except Exception as e:
             print(f"Ошибка keep-alive: {e}")
         threading.Timer(10, Toolset.keep_alive).start()
