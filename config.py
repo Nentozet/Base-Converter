e5390e1db8ce0e79beda96a29956326230ebf77a
Nentozet <160948397+Nentozet@users.noreply.github.com>
2025-02-02 13:27:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,29 @@ class Config:
     Correct_Font_Color = "#00b400"
     Conversion_Accuracy = 12
     Calculation_Accuracy = 12
-    Operation_Changer = {
+    Converter_Arg_Names_Changer = {
+        'fn': 'from_number',
+        'fb': 'from_base',
+        'tb': 'to_base',
+        'accuracy': 'accuracy'
+    }
+    Calculator_Arg_Names_Changer = {
+        'n1': 'number1',
+        'b1': 'base1',
+        'n2': 'number2',
+        'b2': 'base2',
+        'oper': 'operation',
+        'calc_b': 'calculation_base',
+        'accuracy': 'accuracy'
+    }
+    Operation_Args_Changer = {
         'add': '+',
         'sub': '-',
         'mult': '*',
-        'div': '/'
+        'div': '/',
+    }
+
+    Task_Arg_Names_Changer = {
+        'type': 'task_type',
+        'lang': 'lang'
     }
